1dbdb99ca6efb2929fd7a77b2cdb4d6df5feeb1c
samaosama <100531862+samaosama@users.noreply.github.com>
2023-07-24 19:18:15 +0300

    cw4 & bonus done

--- bonus/lib/main.dart
+++ bonus/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +8,249 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: const Color.fromARGB(255, 178, 141, 228),
+          actions: [
+            const Center(child: Icon(Icons.qr_code_scanner_sharp)),
+            const SizedBox(
+              width: 110,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Center(
+                child: Text(
+              'العاصمة',
+              style: GoogleFonts.elMessiri(
+                  textStyle: const TextStyle(fontSize: 20)),
+            )),
+            const SizedBox(
+              width: 100,
+            ),
+            const Center(child: Icon(Icons.settings)),
+            const SizedBox(
+              width: 20,
             ),
           ],
         ),
+        body: Column(
+          children: [
+            Center(
+              child: Container(
+                padding: const EdgeInsets.only(top: 16),
+                width: 500,
+                color: const Color.fromARGB(255, 178, 141, 228),
+                child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('12:28',
+                          style: GoogleFonts.elMessiri(
+                              textStyle: const TextStyle(
+                                  fontSize: 80, color: Colors.white))),
+                      Text('38',
+                          style: GoogleFonts.elMessiri(
+                              textStyle: const TextStyle(
+                                  fontSize: 30, color: Colors.white)))
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(top: 20),
+              color: const Color.fromARGB(255, 178, 141, 228),
+              width: 500,
+              child: Center(
+                child: Text('باقي على الأذان',
+                    style: GoogleFonts.elMessiri(
+                        textStyle: const TextStyle(
+                            fontSize: 20, color: Colors.white))),
+              ),
+            ),
+            Container(
+              color: const Color.fromARGB(255, 178, 141, 228),
+              width: 500,
+              padding: const EdgeInsets.only(top: 40),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Padding(padding: EdgeInsets.only(left: 10)),
+                  const Icon(
+                    Icons.keyboard_arrow_left_outlined,
+                    color: Colors.white,
+                  ),
+                  const Padding(padding: EdgeInsets.only(left: 50)),
+                  Text(' فبراير 21 - 20 رجب',
+                      style: GoogleFonts.elMessiri(
+                          textStyle: const TextStyle(
+                              fontSize: 20, color: Colors.white))),
+                  const Padding(padding: EdgeInsets.only(right: 60)),
+                  const Icon(
+                    Icons.keyboard_arrow_right_outlined,
+                    color: Colors.white,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              color: const Color.fromARGB(255, 178, 141, 228),
+              width: 500,
+              padding: const EdgeInsets.only(top: 30),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Text('3:37 AM',
+                        style: GoogleFonts.elMessiri(
+                            textStyle: const TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                        ))),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 180.0),
+                    child: Text('الفجر',
+                        style: GoogleFonts.elMessiri(
+                            textStyle: const TextStyle(
+                                fontSize: 20, color: Colors.white))),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              color: const Color.fromARGB(255, 229, 213, 251),
+              width: 500,
+              padding: const EdgeInsets.only(top: 20),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 9, bottom: 9),
+                    child: Text('5:04 AM',
+                        style: GoogleFonts.elMessiri(
+                            textStyle: const TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                        ))),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 180.0, bottom: 9),
+                    child: Text('الشروق',
+                        style: GoogleFonts.elMessiri(
+                            textStyle: const TextStyle(
+                                fontSize: 20, color: Colors.white))),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              color: const Color.fromARGB(255, 178, 141, 228),
+              width: 500,
+              padding: const EdgeInsets.only(top: 20),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16),
+                    child: Text('11:45 AM',
+                        style: GoogleFonts.elMessiri(
+                            textStyle: const TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                        ))),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 180.0),
+                    child: Text('الظهر',
+                        style: GoogleFonts.elMessiri(
+                            textStyle: const TextStyle(
+                                fontSize: 20, color: Colors.white))),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              color: const Color.fromARGB(255, 178, 141, 228),
+              width: 500,
+              padding: const EdgeInsets.only(top: 30),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16),
+                    child: Text('3:21 PM',
+                        style: GoogleFonts.elMessiri(
+                            textStyle: const TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                        ))),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 190.0),
+                    child: Text('العصر',
+                        style: GoogleFonts.elMessiri(
+                            textStyle: const TextStyle(
+                                fontSize: 20, color: Colors.white))),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              color: const Color.fromARGB(255, 178, 141, 228),
+              width: 500,
+              padding: const EdgeInsets.only(top: 30),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16),
+                    child: Text('6:25 PM',
+                        style: GoogleFonts.elMessiri(
+                            textStyle: const TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                        ))),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 180.0),
+                    child: Text('المغرب',
+                        style: GoogleFonts.elMessiri(
+                            textStyle: const TextStyle(
+                                fontSize: 20, color: Colors.white))),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              color: const Color.fromARGB(255, 178, 141, 228),
+              width: 500,
+              height: 83,
+              padding: const EdgeInsets.only(top: 25),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 15),
+                    child: Text('7:50 PM',
+                        style: GoogleFonts.elMessiri(
+                            textStyle: const TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                        ))),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 180.0, bottom: 12),
+                    child: Text('العشاء',
+                        style: GoogleFonts.elMessiri(
+                            textStyle: const TextStyle(
+                                fontSize: 20, color: Colors.white))),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
